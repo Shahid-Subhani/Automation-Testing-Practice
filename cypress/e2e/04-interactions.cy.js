f00a60d4d90d71851b8fc9d7373e5d83b98a5840
Subhani420 <subhanishahid420@gmail.com>
2024-10-12 16:43:06 -0700

    Updated

--- cypress/e2e/04-interactions.cy.js
+++ cypress/e2e/04-interactions.cy.js
@@ -15,7 +15,39 @@ describe('Interactions', ()=>{
     it('Sortable',()=>{
         cy.contains('span', 'Sortable').click();
         cy.get('#demo-tab-grid').click();
-        cy.get('create-grid').contains('one').click();
-
+        cy.get('#demo-tabpane-grid > div > div > div:nth-child(1)').click();
+        cy.get('#demo-tabpane-grid > div > div > div:nth-child(2)').click();
+        cy.get('#demo-tabpane-grid > div > div > div:nth-child(3)').click();
+        cy.get('#demo-tabpane-grid > div > div > div:nth-child(4)').click();
+        cy.get('#demo-tabpane-grid > div > div > div:nth-child(5)').click();
+        cy.get('#demo-tabpane-grid > div > div > div:nth-child(6)').click();
+        cy.get('#demo-tabpane-grid > div > div > div:nth-child(7)').click();
+        cy.get('#demo-tabpane-grid > div > div > div:nth-child(8)').click();
+        cy.get('#demo-tabpane-grid > div > div > div:nth-child(9)').click();
+        cy.get('#demo-tab-list').click();
+        cy.xpath('//*[@id="demo-tabpane-list"]/div/div[1]').click();
+        cy.xpath('//*[@id="demo-tabpane-list"]/div/div[2]').click();
+        cy.xpath('//*[@id="demo-tabpane-list"]/div/div[3]').click();
+        cy.xpath('//*[@id="demo-tabpane-list"]/div/div[4]').click();
+        cy.xpath('//*[@id="demo-tabpane-list"]/div/div[5]').click();
+        cy.xpath('//*[@id="demo-tabpane-list"]/div/div[6]').click();
+    })
+    it('Selectable', ()=>{
+        cy.contains('span', 'Selectable').click();
+        cy.get('#demo-tab-grid').click();
+        cy.xpath('//*[@id="row1"]/li[1]').click();
+        cy.xpath('//*[@id="row1"]/li[2]').click();
+        cy.xpath('//*[@id="row1"]/li[3]').click();
+        cy.xpath('//*[@id="row3"]/li[1]').click();
+        cy.xpath('//*[@id="row3"]/li[2]').click();
+        cy.xpath('//*[@id="row3"]/li[3]').click();
+        cy.xpath('//*[@id="row2"]/li[3]').click();
+        cy.xpath('//*[@id="row2"]/li[2]').click();
+        cy.xpath('//*[@id="row2"]/li[1]').click();
+        cy.get('#demo-tab-list').click();
+        cy.xpath('//*[@id="verticalListContainer"]/li[1]').click();
+        cy.xpath('//*[@id="verticalListContainer"]/li[4]').click();
+        cy.xpath('//*[@id="verticalListContainer"]/li[2]').click();
+        cy.xpath('//*[@id="verticalListContainer"]/li[3]').click();
     })
 })
